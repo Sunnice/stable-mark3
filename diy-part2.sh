2419a3f6bf06ffc49624950985f8f6680b2130b5
Sunnice <44739306+Sunnice@users.noreply.github.com>
2020-09-12 17:47:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,22 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/ElonH/Rclone-OpenWrt.git package/rclone
+git clone https://github.com/fw876/helloworld.git package/helloworld
+git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/koolproxyR
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/adguardhome
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
+git clone https://github.com/skyformat99/eqos.git package/eqos
+git clone https://github.com/frainzy1477/luci-app-clash.git package/clash
+git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic.git package/unblockneteasemusicadvance
+git clone https://github.com/Lienol/openwrt-package.git package/lienol
+git clone https://github.com/Lienol/luci-theme-darkmatter.git package/luci-theme-darkmatter
+git clone https://github.com/mit-ywtm/luci-theme-rosy.git package/luci-theme-rosy
+git clone https://github.com/Mrbai98/luci-theme-atmaterial.git package/luci-theme-atmaterial
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat
+git clone https://github.com/siropboy/luci-theme-btmod.git package/luci-theme-btmod
+git clone https://github.com/kenzok8/luci-theme-ifit.git package/luci-theme-ifit
+git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/luci-theme-infinityfreedom
