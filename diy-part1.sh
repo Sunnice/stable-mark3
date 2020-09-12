1e15614afd959893db8214430e5e24ba7ac00a0d
Sunnice <44739306+Sunnice@users.noreply.github.com>
2020-09-12 17:46:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,24 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+
+./scripts/feeds clean
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+rm -rf package/lean/luci-app-rclone
+rm -rf package/lean/rclone-ng
+rm -rf package/lean/rclone-webui-react
+rm -rf package/lean/rclone
+rm -rf package/lean/luci-app-kodexplorer
+rm -rf feeds/packages/net/smartdns
+
+svn co https://github.com/Lienol/openwrt-packages/trunk/net/smartdns package/net/smartdns	
+
+rm -rf feeds/packages/net/https-dns-proxy
+svn co https://github.com/coolsnowwolf/packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
+
+svn co https://github.com/coolsnowwolf/packages/trunk/libs/nss package/libs/nss
+
+svn co https://github.com/coolsnowwolf/packages/trunk/libs/nspr package/libs/nspr
